17deb934b6f3bf01d7d48b0416f25c6a8340c25a
sudhi7seas <87425107+sudhi7seas@users.noreply.github.com>
2023-06-20 13:50:11 +0200

    Update Extract.py

--- Extract.py
+++ Extract.py
@@ -1,23 +1,26 @@
+#This script extracts a character from images and saves in a text file
+#Pre-requisites are pytesseract and PIL modules needed to be installed
+
 from PIL import Image
 import pytesseract
 import glob
 
+# we need to provide the location of the executable where tesseract is located
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-#print(pytesseract.image_to_string(Image.open('image1.jpg')))
-#path = glob.glob("C:\\Users\\saptassu\\PycharmProjects1\\pythonProject_1\\*.jpg")
+#the below line extracts the required files from the desired folder
 path = glob.glob("C:\\Work\\Obsolete\\*.jpg")
-#directory = "C://Users/saptassu/PycharmProjects1/pythonProject_1"
-#print(path)
-with open('file.txt', 'w') as file:
-    for file_name in path:
-        #try:
-        print (file_name)
-        img = Image.open(file_name)
-        print(img)
-        text = pytesseract.image_to_string(img)
-        print(text)
 
-        file.write(text)
-    #except Exception:
-        #pass
+#In order to write the contents to the new file, create a new file 'file.txt'
+with open('file.txt', 'w') as file:
+  #iterate through the files that are present in the location indicated by 'path'
+  for file_name in path:
+    print(file_name)
+    img = Image.open(file_name)
+    print(img)
+    text = pytesseract.image_to_string(img)
+    print(text)
+
+    #write the content to the text file
+    file.write(text)
